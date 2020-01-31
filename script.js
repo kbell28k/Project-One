1e66c338e95de666159cf5855306e72e93da9ae0
Mona Lisa <kbell28k@gmail.com>
2020-01-30 21:04:53 -0700

    button changes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// setup variables
-// ==================
-var authKey = "UGdG4OgfnsYV1yqkgxQO8Q4jYUs9EKOK";
-
-// url of api authorization
-var queryURLBase = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=UGdG4OgfnsYV1yqkgxQO8Q4jYUs9EKOK" + authKey;
-
-// variable to track number of articles
-var venueCounter = 0;
+function myFunction() {
+    var x = document.getElementById("allcards");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
